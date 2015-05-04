665d00138f2d042e8db71a27bf73f88da047b681
Spencer Sharpe <jspencersharpe@gmail.com>
2015-05-04 09:25:26 -0500

    cleaned up code, styled, hide table until clicked

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+  $('table').addClass('hidden');
+});
 function matrixCreator(x, y) {
   var matrix=[];
   for(var i=0; i<x; i++) {
@@ -16,9 +19,6 @@ $(document).ready(function(){
   function generateGrid(matrix){
     var $table = $('#grid');
     $($table).empty();
-    // matrix => [0, 0, 0]
-    //           [1, 1, 1]
-    //           [0, 0, 0]
 
     matrix.forEach(function(row){ // first time, row => [0, 0, 0]
       // create a tr for the row
@@ -26,11 +26,9 @@ $(document).ready(function(){
       row.forEach(function(cell){ // first time, cell => 0
         // cell goes into a new td
         // that td goes into a tr
-        var $td = createTableCell(cell);
+      var $td = createTableCell(cell);
         $($td).text(cell);
         $($tr).append($td);
-        // alternative:
-        // $tr.appendChild( createTableCell(cell) );
       });
       // add that tr to the table
       $($table).append($tr);
@@ -40,7 +38,7 @@ $(document).ready(function(){
   function createTableCell(value){
     var $td = $('<td></td>');
     // Apply alive or dead class to the td
-    if(value === 1){
+    if(value === 1) {
       $($td).addClass('alive');
     } else {
       $($td).addClass('dead');
@@ -51,46 +49,45 @@ $(document).ready(function(){
   function livingNeighborCount(x, y) {
     var neighbors = 0;
         for(var i = x-1; i<=x+1; i++) {
-                if (i < 0 || i ===matrix.length) {     
-                } else {
-                    for (var j = y-1; j<=y+1; j++) {
-                    if (j < 0 || i ===matrix[i].length || (i===x && j===y)) {
-                } else {
+            if (i < 0 || i ===matrix.length) {     
+            } else {
+                for (var j = y-1; j<=y+1; j++) {
+                   if (j < 0 || i ===matrix[i].length || (i===x && j===y)) {
+                } else {    
                     if (matrix[i][j] === 1) {
                       neighbors += 1;
-                   }
-                }     
-              }
+                    }
+                  }     
+                }
             }
-          } return neighbors;
-}
+         } 
+        return neighbors;
+  }
 
   
   function calculateNextState(currentState){
     var nextState = [];
     currentState.forEach(function(currentRow, x){
       var nextRow = [];
-      currentRow.forEach(function(currentCell, y){
-        var nextCellState = livingNeighborCount(x, y);
-        // check if current cell alive
-          if (currentState[x][y] === 1) {
-           // if neighbors are greater than 2 or less than 3
-              if (nextCellState < 2 || nextCellState  > 3) {
-
+    currentRow.forEach(function(currentCell, y){
+      var nextCellState = livingNeighborCount(x, y);
+       // check if current cell alive
+       if (currentState[x][y] === 1) {
+         // if neighbors are greater than 2 or less than 3
+           if (nextCellState < 2 || nextCellState  > 3) {
               nextCellState  = 0;
-
-              } else { nextCellState = 1;
-              }        
+           } else { 
+             nextCellState = 1;
+           }        
                 
-            } else {
-               if (nextCellState  === 3) {
-                  nextCellState  = 1;
-               } else {
-                   nextCellState  = 0;    
+       } else {
+           if (nextCellState  === 3) {
+               nextCellState  = 1;
+           } else {
+               nextCellState  = 0;    
          }
-        }
-
-        nextRow.push(nextCellState);
+       }
+       nextRow.push(nextCellState);
       });
       nextState.push(nextRow);
     });
@@ -99,10 +96,10 @@ $(document).ready(function(){
 
  $("#tick").on('click', function(){
     // Tick button has been pressed
+    $('table').removeClass('hidden');
     setInterval(function () {
      matrix = calculateNextState(matrix);
-    generateGrid(matrix);
-  
+     generateGrid(matrix);  
     }, 10);     
     }); 
 });
